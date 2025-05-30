93ae2b4098173bcea0f8f0048d935c4f90e2f80d
Georgios Kokkinos <gk769@bath.ac.uk>
2025-05-30 10:31:48 +0100

    Added Questions PDF, solved questions until Nested Querries

--- main.sql
+++ main.sql
@@ -1,46 +1,100 @@
 
 
 CREATE TABLE Item (
-	  ItemName VARCHAR (30) NOT NULL,
+	ItemName VARCHAR (30) NOT NULL,
   ItemType CHAR(1) NOT NULL,
   ItemColour VARCHAR(10),
   PRIMARY KEY (ItemName));
 
 CREATE TABLE Employee (
-  EmployeeNumber SMALLINT UNSIGNED NOT NULL ,
+  EmployeeNumber SMALLINT NOT NULL ,
   EmployeeName VARCHAR(10) NOT NULL ,
-  EmployeeSalary INTEGER UNSIGNED NOT NULL ,
-  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
-  BossNumber SMALLINT UNSIGNED NOT NULL REFERENCES Employee,
-  PRIMARY KEY (EmployeeNumber));
+  EmployeeSalary INTEGER NOT NULL ,
+  DepartmentName VARCHAR(10) NOT NULL,
+  BossNumber SMALLINT NOT NULL,
+  PRIMARY KEY (EmployeeNumber),
+  FOREIGN KEY (DepartmentName) REFERENCES Department(DepartmentName),
+  FOREIGN KEY (BossNumber) REFERENCES Employee(EmployeeNumber));
 
 CREATE TABLE Department (
   DepartmentName VARCHAR(10) NOT NULL,
-  DepartmentFloor SMALLINT UNSIGNED NOT NULL,
-  DepartmentPhone SMALLINT UNSIGNED NOT NULL,
-  EmployeeNumber SMALLINT UNSIGNED NOT NULL REFERENCES 
-    Employee,
-  PRIMARY KEY (DepartmentName));
+  DepartmentFloor SMALLINT NOT NULL,
+  DepartmentPhone SMALLINT NOT NULL,
+  EmployeeNumber SMALLINT NOT NULL,
+  PRIMARY KEY (DepartmentName),
+  FOREIGN KEY (EmployeeNumber) REFERENCES Employee(EmployeeNumber));
+
 
 CREATE TABLE Sale (
-  SaleNumber INTEGER UNSIGNED NOT NULL,
-  SaleQuantity SMALLINT UNSIGNED NOT NULL DEFAULT 1,
-  ItemName VARCHAR(30) NOT NULL REFERENCES Item,
-  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
-  PRIMARY KEY (SaleNumber));
+  SaleNumber INTEGER NOT NULL,
+  SaleQuantity SMALLINT NOT NULL DEFAULT 1,
+  ItemName VARCHAR(30) NOT NULL,
+  DepartmentName VARCHAR(10) NOT NULL,
+  PRIMARY KEY (SaleNumber),
+  FOREIGN KEY (ItemName) REFERENCES Item(ItemName),
+  FOREIGN KEY (DepartmentName) REFERENCES Department(DepartmentName));
 
 CREATE TABLE Supplier (
-  SupplierNumber INTEGER UNSIGNED NOT NULL,
+  SupplierNumber INTEGER NOT NULL,
   SupplierName VARCHAR(30) NOT NULL,
   PRIMARY KEY (SupplierNumber));
 
 CREATE TABLE Delivery (
-  DeliveryNumber INTEGER UNSIGNED NOT NULL,
-  DeliveryQuantity SMALLINT UNSIGNED NOT NULL DEFAULT 1,
-  ItemName VARCHAR(30) NOT NULL REFERENCES Item,
-  DepartmentName VARCHAR(10) NOT NULL REFERENCES Department,
-  SupplierNumber INTEGER UNSIGNED NOT NULL REFERENCES  
-     Supplier,
-  PRIMARY KEY (DeliveryNumber));
+  DeliveryNumber INTEGER NOT NULL,
+  DeliveryQuantity SMALLINT NOT NULL DEFAULT 1,
+  ItemName VARCHAR(30) NOT NULL,
+  DepartmentName VARCHAR(10) NOT NULL,
+  SupplierNumber INTEGER NOT NULL,
+  PRIMARY KEY (DeliveryNumber),
+  FOREIGN KEY (ItemName) REFERENCES Item(ItemName),
+  FOREIGN KEY (DepartmentName) REFERENCES Department(DepartmentName),
+  FOREIGN KEY (SupplierNumber) REFERENCES Supplier(SupplierNumber));
+
 
 -- using the data in the text files, insert into the tables this information
+-- What i did was 
+-- .mode tab
+-- import XXX.txt XXX (.e.g .import item.txt Item
+
+-- Name of employees from marketing departiment 
+
+SELECT EmployeeName FROM Employee WHERE DepartmentName = 'Marketing';
+
+-- Find the items sold by the departments on the second floor.
+
+SELECT ItemName FROM Sale,Department WHERE Sale.DepartmentName = Department.DepartmentName AND Department.DepartmentFloor= 2;
+
+-- test natural join of above . It removes duplicates.
+
+SELECT DISTINCT ItemName
+FROM (Sale NATURAL JOIN Department)
+WHERE Department.DepartmentFloor = 2;
+
+-- test join of above
+
+SELECT DISTINCT ItemName
+FROM (Sale JOIN Department)
+WHERE Department.DepartmentFloor = 2;
+
+--  Identify by floor the items available on floors other than the second floor
+Select DISTINCT ItemName, DepartmentFloor AS 'Item on floor' FROM Sale, Department WHERE Sale.DepartmentName = Department.DepartmentName AND Department.DepartmentFloor != 2;
+
+-- 4. Find the average salary of the employees in the Clothes department
+SELECT AVG(EmployeeSalary) FROM Employee WHERE DepartmentName = 'Clothes';
+
+-- Find, for each department, the average salary of the employees in that department and report by descending salary.
+
+SELECT DepartmentName, AVG(EmployeeSalary)AS Avg_Salary FROM Employee GROUP BY DepartmentName ORDER BY Avg_Salary DESC;
+
+-- List the items delivered by exactly one supplier (i.e. the items always delivered by the same supplier).
+
+SELECT DISTINCT ItemName, SupplierName FROM Delivery, Supplier WHERE Delivery.SupplierNumber = Supplier.SupplierNumber GROUP BY ItemName HAVING COUNT(DISTINCT SupplierName) = 1;
+
+--  List the suppliers that deliver at least 10 items.
+
+SELECT SupplierName, COUNT(DISTINCT ItemName) AS Number_of_Items FROM Delivery, Supplier WHERE Delivery.SupplierNumber = Supplier.SupplierNumber GROUP BY SupplierName HAVING COUNT(DISTINCT ItemName) >= 10;
+
+-- . What is the average delivery quantity of items of type 'N' delivered by each company
+
+SELECT Item.ItemType, Supplier.SupplierName, AVG(DeliveryQuantity) FROM Item, Delivery, Supplier WHERE Item.ItemName = Delivery.ItemName AND Supplier.SupplierNumber=Delivery.SupplierNumber AND ItemType = 'N' GROUP BY SupplierName;
+
